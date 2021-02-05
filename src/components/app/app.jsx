1f9f89828a87c83133df170f9476e10ddd1233f4
vinoven <mikhailsechin@gmail.com>
2021-02-06 00:53:34 +0300

    Подключет React Router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MainPage from '../main-page/main-page';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App = (props) => {
   const {films, promo} = props;
