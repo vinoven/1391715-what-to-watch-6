de80d33f431c4656393a047385a8840857715830
vinoven <mikhailsechin@gmail.com>
2021-02-06 01:19:08 +0300

    Настраивает маршрутизацию по основным страницам

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -12,7 +12,7 @@ const Player = () => {
         <div className="player__controls-row">
           <div className="player__time">
             <progress className="player__progress" value="30" max="100"></progress>
-            <div className="player__toggler" style="left: 30%;">Toggler</div>
+            <div className="player__toggler" style={{left: 30}}>Toggler</div>
           </div>
           <div className="player__time-value">1:30:29</div>
         </div>
@@ -33,8 +33,8 @@ const Player = () => {
             <span>Full screen</span>
           </button>
         </div>
-      </div >
-    </div >);
+      </div>
+    </div>);
 };
 
 export default Player;
